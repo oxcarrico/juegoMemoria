b98bc57434c3e1636fe8ff440a6ac37685855598
oxcarrico <146721536+oxcarrico@users.noreply.github.com>
2023-12-14 22:13:37 +0100

    Add files via upload

--- TareaIos/finJuego.swift
+++ TareaIos/finJuego.swift
@@ -1,27 +1,28 @@
 import UIKit
 
 class finJuego: UIViewController {
-    var puntu: [Int] = []
-    
+    var puntuaciones: Int = 0  
+
     @IBOutlet weak var puntuacionesLabel: UILabel!
     @IBOutlet weak var puntuacionMaximaLabel: UILabel!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         actualizarPuntuacionesLabel()
         actualizarPuntuacionMaxima()
         guardarPuntuacionLocalmente()
         puntuar()
-        print(puntu)
+        print(puntuaciones)
         UserDefaults.standard.object(forKey: "PP")
+
+        enviarPuntuacionAPI(puntuacion: puntuaciones)  // Enviar la puntuación al terminar la partida
     }
-    
+
     func actualizarPuntuacionesLabel() {
         puntuacionesLabel.text = "Puntuacion de partida: \(puntuaciones)"
     }
-    
+
     func actualizarPuntuacionMaxima() {
         let puntuacionMaxima = UserDefaults.standard.integer(forKey: "PuntuacionMaxima")
         if puntuaciones > puntuacionMaxima {
@@ -31,12 +32,46 @@ class finJuego: UIViewController {
             puntuacionMaximaLabel.text = "Puntuación Máxima: \(puntuacionMaxima)"
         }
     }
-    @IBAction func puntuar(){
-        puntu.append(puntuaciones)
+
+    @IBAction func puntuar() {
+        
         UserDefaults.standard.set(puntuaciones, forKey: "PP")
     }
+
     func guardarPuntuacionLocalmente() {
-       
         UserDefaults.standard.set(puntuaciones, forKey: "PuntuacionActual")
+    }
+
+    func enviarPuntuacionAPI(puntuacion: Int) {
+        guard let url = URL(string: "https://proyectojuegodememoria-default-rtdb.europe-west1.firebasedatabase.app/.json") else {
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let jsonData: [String: Any] = ["puntuacion": puntuacion]
+
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: jsonData)
+        } catch let error {
+            print("Error serializing JSON: \(error)")
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error en la solicitud: \(error)")
+                return
+            }
+
+            if let data = data {
+                let responseString = String(data: data, encoding: .utf8)
+                print("Respuesta de la API: \(responseString ?? "")")
+            }
+        }
+
+        task.resume()
     }
 }
--- TareaIos/puntuacionesView.swift
+++ TareaIos/puntuacionesView.swift
@@ -1,32 +1,56 @@
 import UIKit
 
-class puntuacionesView: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    var puntuacionesGuardadas: [Int] = [1,3,4]
+class PuntuacionesView: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var puntuacionesGuardadas: [Int] = []
 
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+
         tableView.dataSource = self
         tableView.delegate = self
 
-       
-        cargarPuntuacionesGuardadas()
-        print(puntuacionesGuardadas)
+        cargarPuntuacionesDesdeAPI()
     }
 
-    func cargarPuntuacionesGuardadas() {
-        if let puntuacionActual = UserDefaults.standard.object(forKey: "PuntuacionActual") as? Int {
-            puntuacionesGuardadas.append(puntuacionActual)
-        }
-        
-    }
-    func guardadosPuntuaciones() {
-        let puntuacionguardada = UserDefaults.standard.integer(forKey: "Puntuacionguardada")
-            UserDefaults.standard.set(puntuaciones, forKey: "Puntuacionguardada")
+    func cargarPuntuacionesDesdeAPI() {
+        guard let url = URL(string: "https://proyectojuegodememoria-default-rtdb.europe-west1.firebasedatabase.app/.json") else {
+            return
         }
 
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("Error en la solicitud: \(error)")
+                return
+            }
+
+            guard let data = data else {
+                print("Datos no válidos")
+                return
+            }
+
+            do {
+           
+                let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+
+               
+                if let puntuaciones = json?["puntuaciones"] as? [Int] {
+                  
+                    self.puntuacionesGuardadas = puntuaciones
+
+                    
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+            } catch let error {
+                print("Error al decodificar JSON: \(error)")
+            }
+        }
+
+        task.resume()
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return puntuacionesGuardadas.count
@@ -35,13 +59,10 @@ class puntuacionesView: UIViewController, UITableViewDataSource, UITableViewDele
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "puntuacionCell", for: indexPath) as UITableViewCell
 
-        
         cell.textLabel?.text = "Puntuación: \(puntuacionesGuardadas[indexPath.row])"
 
         return cell
     }
-    @IBAction func quitar(_ sender: UIButton) {
-        puntuaciones = 0
-    }
-    
+
+ 
 }
